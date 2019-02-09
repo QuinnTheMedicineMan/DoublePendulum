c032db6aeed1071e241071f24cf1f543ac5ab8a9
Liam Quinn <lquinn@km3net.de>
2019-02-09 13:15:29 +0000

    Added equations of state for double pendulum.

--- SimpleHarmonicOscillator.py
+++ SimpleHarmonicOscillator.py
@@ -40,4 +40,28 @@ class DoublePendulum:
 
 
     # Call operator: Equations of state for the double pendulum
+    # t: time [seconds]
+    # x: [theta 1 [rad], theta 2 [rad], dL/d(dtheta1/dt)) [second/rad], dL/d(dtheta2/dt)) [second, rad]]
+    # return derivative of x
     def __call__(self, t, x):
+        theta1 = x[0]   # First  angle in radians
+        theta2 = x[1]   # Second angle in radians
+        p1     = x[2]   # Partial derivative of the lagrangian with respect to the time derivative of the first  angle
+        p2     = x[3]   # Partial derivative of the lagrangian with respect to the time derivative of the second angle
+
+        # Potential for speed up / avoidance of floating point errors in the cosine
+        theta1_dot = (2.0*p1 - 3.0*np.cos(theta1-theta2)*p2) / (16.0 - 9*np.cos(theta1-theta2)*np.cos(theta1-theta2))
+        theta2_dot = (8.0*p2 - 3.0*np.cos(theta1-theta2)*p1) / (16.0 - 9*np.cos(theta1-theta2)*np.cos(theta1-theta2))
+
+        theta1_dot *= 6.0/(self.len*self.len*self.mass)
+        theta2_dot *= 6.0/(self.len*self.len*self.mass)
+
+        dL_dtheta1 =  theta1_dot*theta2_dot*np.sin(theta1-theta2) + (3.0*Constants.g_acceleration/self.len) * np.sin(theta1)
+        dL_dtheta2 = -theta1_dot*theta2_dot*np.sin(theta1-theta2) + (Constants.g_acceleration/self.len) * np.sin(theta2)
+
+        dL_dtheta1 *= -0.5*self.mass*self.len*self.len
+        dL_dtheta2 *= -0.5*self.mass*self.len*self.len
+
+        return [theta1_dot, theta2_dot, dL_dtheta1, dL_dtheta2]
+
+
