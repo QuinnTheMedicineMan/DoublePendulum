6de82d08082b46e5afc002f7c5cf04671752e85b
Liam Quinn <lquinn@km3net.de>
2019-02-11 12:51:09 +0000

    Added a sanity check for double pendulum.

--- test.py
+++ test.py
@@ -7,11 +7,13 @@ import SimpleHarmonicOscillator as sho
 
 def main():
 
+
+    # Simple harmonic pendulum checks
     # Solve numerically
     t   = np.linspace(0, 30, 3000)
     pen = sho.SimpleHarmonicPendulum()
 
-    sol = pen.solve([1.0, 0.0], t)
+    sol = pen.solve([np.radians(10.0), 0.0], t)
 
     plt.plot(sol[0], np.degrees(sol[1]), label="Displacement [deg]")
     plt.plot(sol[0], np.degrees(sol[2]), label="Velocity     [deg/s]")
@@ -23,7 +25,7 @@ def main():
     plt.show()
 
     # Calculate explicitly
-    exp = pen.explicit([1.0, 0.0], t)
+    exp = pen.explicit(np.radians(10.0), t)
 
     plt.plot(sol[0], sol[1] - exp[1], label="Displacement error [deg]")
     plt.plot(sol[0], sol[2] - exp[2], label="Velocity error     [deg/s]")
@@ -31,6 +33,33 @@ def main():
 
     plt.title("Simple Harmonic Pendulum Errors")
     plt.xlabel("Time [s]")
+    plt.legend()
+    plt.show()
+
+
+    # Double pendulum checks
+    # Does a pendulum at rest stay put?
+    pen = sho.DoublePendulum()
+
+    sol = pen.solve([0.0, 0.0, 0.0, 0.0], t)
+
+    plt.plot(sol[0], np.degrees(sol[4]), label=r"$\theta_1$ [deg]")
+    plt.plot(sol[0], np.degrees(sol[5]), label=r"$\theta_2$ [deg]")
+
+    plt.title("Double Pendulum at Rest")
+    plt.xlabel("Time [s]")
+    plt.legend()
+    plt.show()
+
+
+    # Make sure total energy of the system is conserved
+    plt.plot(sol[0], sol[1], label="Total Energy",     color='black')
+    plt.plot(sol[0], sol[2], label="Kinetic Energy",   color='red')
+    plt.plot(sol[0], sol[3], label="Potential Energy", color='blue')
+
+    plt.title("Double Pendulum at Rest")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Energy [J]")
     plt.legend()
     plt.show()
 
