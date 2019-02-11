Fixed sign error?

SimpleHarmonicOscillator.py
@@ -108,8 +108,8 @@ class DoublePendulum:
         theta1_dot = self.__get_theta1_dot(theta1, theta2, p1, p2)
         theta2_dot = self.__get_theta2_dot(theta1, theta2, p1, p2)
 
-        dL_dtheta1 =  theta1_dot*theta2_dot*np.sin(theta1-theta2) + (3.0*Constants.g_acceleration/self.len) * np.sin(theta1)
-        dL_dtheta2 = -theta1_dot*theta2_dot*np.sin(theta1-theta2) + (Constants.g_acceleration/self.len) * np.sin(theta2)
+        dL_dtheta1 =  theta1_dot*theta2_dot*np.sin(theta1-theta2) + (-3.0*Constants.g_acceleration/self.len) * np.sin(theta1)
+        dL_dtheta2 = -theta1_dot*theta2_dot*np.sin(theta1-theta2) + (-Constants.g_acceleration/self.len) * np.sin(theta2)
 
         dL_dtheta1 *= -0.5*self.mass*self.len*self.len
         dL_dtheta2 *= -0.5*self.mass*self.len*self.len